a58f4f7adf50ffae3370ea2dbf46559fca5dedb6
trentandraka <stealthbomber10@gmail.com>
2017-09-01 02:17:57 -0400

    disable raw and canonical mode

--- samus.c
+++ samus.c
@@ -1,11 +1,19 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-void enableRawMode() {
-  struct termios raw;
+struct termios orig_termios;
 
-  tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+
+  raw.c_lflag &= ~(ECHO | ICANON);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
