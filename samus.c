6ff8ce2f6b80e5568adbeb42bdcbb376e0bb4b9c
trentandraka <stealthbomber10@gmail.com>
2017-09-01 03:17:37 -0400

    prints ascii to screen

--- samus.c
+++ samus.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -13,7 +15,7 @@ void enableRawMode() {
   atexit(disableRawMode);
   struct termios raw = orig_termios;
 
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -22,6 +24,12 @@ int main() {
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
   return 0;
 }
