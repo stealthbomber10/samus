683403c0ed66f838afcf79a7c346dba5f4446eab
trentandraka <stealthbomber10@gmail.com>
2017-09-05 22:52:37 -0400

    home and delete added

--- samus.c
+++ samus.c
@@ -21,6 +21,9 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN
 };
@@ -98,10 +101,20 @@ int editorReadKey() {
         }
         if (seq[2] == '~') {
           switch (seq[1]) {
+            case '1':
+              return HOME_KEY;
+            case '3':
+              return DEL_KEY;
+            case '4':
+              return END_KEY;
             case '5':
               return PAGE_UP;
             case '6':
               return PAGE_DOWN;
+            case '7':
+              return HOME_KEY;
+            case '8':
+              return END_KEY;
           }
         }
       } else {
@@ -114,7 +127,18 @@ int editorReadKey() {
               return ARROW_RIGHT;
             case 'D':
               return ARROW_LEFT;
+            case 'H':
+              return HOME_KEY;
+            case 'F':
+              return END_KEY;
           }
+        }
+      } else if (seq[0] == '0') {
+        switch (seq[1]) {
+          case 'H':
+            return HOME_KEY;
+          case 'F':
+            return END_KEY;
         }
       }
 
@@ -281,6 +305,14 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
+      break;
+
+    case HOME_KEY:
+      E.cx = 0;
+      break;
+
+    case END_KEY:
+      E.cx = E.screencols - 1;
       break;
 
     case PAGE_UP:
